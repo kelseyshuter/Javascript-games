1b69cfb0271d419fd628d14c8ca412e254ad0d1f
kelseyshuter <kshuter21@gmail.com>
2022-08-19 23:52:16 -0400

    improved rock paper scissors (RPS)

--- Rock-paper-scissors/app.js
+++ Rock-paper-scissors/app.js
@@ -1,14 +1,18 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice 
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const playerChoiceDisplay = document.getElementById('player-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+const playerBox = document.querySelector('.player');
+const computerBox = document.querySelector('.computer')
+let playerChoice 
 let computerChoice
 let result
+let playerWins = false
+let gameTied = false
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
+    playerChoice = e.target.id
+    playerChoiceDisplay.innerHTML = playerChoice
     generateComputerChoice()
     getResult()
 }))
@@ -28,27 +32,58 @@ function generateComputerChoice() {
     computerChoiceDisplay.innerHTML = computerChoice
 }
 
+function winnerColor() {
+    if (gameTied === false) {
+        if (playerWins === true) {
+            playerBox.classList.add('winner')
+        } else {
+            computerBox.classList.add('winner')
+        }    
+    } else {
+        playerBox.classList.add('winner')
+        computerBox.classList.add('winner')
+    }
+    setTimeout(() => {playerBox.classList.remove('winner');}, 1000)
+    setTimeout(() => {computerBox.classList.remove('winner');}, 1000)
+}
+
+
 function getResult () {
-    if (computerChoice === userChoice) {
+    if (computerChoice === playerChoice) {
         result = 'its a draw!'
+        gameTied = true
+        playerWins = false
     }
-    if (computerChoice === 'rock' && userChoice === 'paper') {
+    if (computerChoice === 'rock' && playerChoice === 'paper') {
         result = 'you won!'
+        gameTied = false
+        playerWins = true
     }
-    if (computerChoice === 'rock' && userChoice === 'scissors') {
+    if (computerChoice === 'rock' && playerChoice === 'scissors') {
         result = 'you lost!'
+        gameTied = false
+        playerWins = false
     }
-    if (computerChoice === 'paper' && userChoice === 'rock') {
+    if (computerChoice === 'paper' && playerChoice === 'rock') {
         result = 'you lost!'
+        gameTied = false
+        playerWins = false
     }
-    if (computerChoice === 'paper' && userChoice === 'scissors') {
+    if (computerChoice === 'paper' && playerChoice === 'scissors') {
         result = 'you won!'
+        gameTied = false
+        playerWins = true
     }
-    if (computerChoice === 'scissors' && userChoice === 'rock') {
+    if (computerChoice === 'scissors' && playerChoice === 'rock') {
         result = 'you won!'
+        gameTied = false
+        playerWins = true
     }
-    if (computerChoice === 'scissors' && userChoice === 'paper') {
+    if (computerChoice === 'scissors' && playerChoice === 'paper') {
         result = 'you lost!'
+        gameTied = false
+        playerWins = false
     }
     resultDisplay.innerHTML = result
+    winnerColor()
 }
