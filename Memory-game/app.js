633847cb3c6c6f387f36e18be74db779a9749e3a
kelseyshuter <kshuter21@gmail.com>
2022-08-23 14:59:52 -0400

    styled and updated memory game

--- Memory-game/app.js
+++ Memory-game/app.js
@@ -49,55 +49,83 @@ const cardArray = [
     }
 ]
 
-cardArray.sort(() => 0.5 - Math.random())
+cardArray.sort(() => 0.5 - Math.random()) 
 
 const gridDisplay = document.querySelector('#grid')
-resultDisplay = document.querySelector('#result')
+const attemptDisplay = document.querySelector('#attempt')
+const resultDisplay = document.querySelector('#result')
+const messageBox = document.querySelector('.message-box')
+const restartButton = document.getElementById('restart-button')
 let cardsChosen = []
 let cardsChosenIds = []
 let cardsWon = []
+let attempt = 0
+
+
 
 function createBoard () {
-    for (let i =0; i < cardArray.length; i++) {
-        const card = document.createElement('img')
+    for (let i =0; i < cardArray.length; i++) {              
+        const card = document.createElement('img') 
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
+        card.setAttribute('class', 'card')
         card.addEventListener('click', flipCard)
-        gridDisplay.appendChild(card)        
+        gridDisplay.appendChild(card)
     }
 }
 createBoard()
+
+function restartGame() {
+    cardsChosen = []
+    cardsChosenIds = []
+    cardsWon = []
+    attempt = 0
+    attemptDisplay.innerHTML = attempt
+    resultDisplay.textContent = cardsWon.length
+    restartButton.textContent = 'RESTART'
+    messageBox.innerHTML = 'Find the matching pairs!'
+    for (let i = 0; i < cardArray.length; i++) {
+        let element = document.querySelector("[data-id='" + i + "']")
+        gridDisplay.removeChild(element)        
+    }
+    cardArray.sort(() => 0.5 - Math.random()) 
+    createBoard()
+}
+
+restartButton.addEventListener('click', restartGame)
 
 function checkMatch() {
     const cards = document.querySelectorAll('img')
     const optionOneId = cardsChosenIds[0]
     const optionTwoId = cardsChosenIds[1]
-    console.log(cards)
-    console.log('check for match!')
-    if (optionOneId == optionTwoId) {
+    attempt += 1
+    attemptDisplay.innerHTML = attempt
+
+    if (optionOneId === optionTwoId) {
         cards[optionOneId].setAttribute('src', 'images/blank.png')
         cards[optionTwoId].setAttribute('src', 'images/blank.png')
-        alert('You clicked the same card')
-    }
-    
-    if (cardsChosen[0] == cardsChosen[1]) {
-        alert('You found a match!')
+        messageBox.innerHTML = 'You clicked the same card!'
+    } else if (cardsChosen[0] === cardsChosen[1]) {
+        messageBox.innerHTML = 'You found a match!'
         cards[optionOneId].setAttribute('src', 'images/white.png')
         cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].setAttribute('class', 'move-back')
+        cards[optionTwoId].setAttribute('class', 'move-back')
         cards[optionOneId].removeEventListener('click', flipCard)
         cards[optionTwoId].removeEventListener('click', flipCard)
         cardsWon.push(cardsChosen)
     } else {
         cards[optionOneId].setAttribute('src', 'images/blank.png')
         cards[optionTwoId].setAttribute('src', 'images/blank.png')
-        alert('Try again!')
+        messageBox.innerHTML = 'Try Again!'
     }
     resultDisplay.textContent = cardsWon.length
     cardsChosen = []
     cardsChosenIds = []
 
     if (cardsWon.length === cardArray.length/2) {
-        resultDisplay.textContent = 'Congratulations! You found them all'
+        messageBox.textContent = 'Congratulations! You found them all'
+        restartButton.textContent = 'PLAY AGAIN'
     }
 
 }
@@ -107,13 +135,12 @@ function flipCard() {
     cardArray[cardId]
     cardsChosen.push(cardArray[cardId].name)
     cardsChosenIds.push(cardId)
-    console.log(cardsChosen)
-    console.log(cardsChosenIds)
     this.setAttribute('src', cardArray[cardId].img)
     if (cardsChosen.length === 2) {
         setTimeout(checkMatch, 500)
     }
 }
+
 
 
 
